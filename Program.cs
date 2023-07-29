eac444ae5b81f99ce07051099168ffc1087ee8f3
abdelrahmanAmmar53 <marbdrhman2@gmail.com>
2023-07-29 20:59:52 +0300

    book

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using bank;
+ 
+book book = new FictionBook();
+book.author= "dan brown" ;
+book.Number= 333;
+Console.WriteLine(book.author);
+Console.WriteLine(book.Number);
+book book1 = new nonFictionBook();
+book1.author="khaled tawfic";
+book1.title="supernatural";
+Console.WriteLine(book1.author);
+Console.WriteLine(book1.title);
